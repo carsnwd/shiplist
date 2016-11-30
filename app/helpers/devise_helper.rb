3d4a50637481211a8d608c1bfe37663d0c686e2c
carsnwd <carsnwd@gmail.com>
2016-11-30 08:30:09 -0500

    Sign up works again, devise helper broke it before

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,6 +1,6 @@
 module DeviseHelper
 	def devise_error_messages!
-		if resources.errors.empty?
+		if resource.errors.empty?
 			return
 		end
 		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
